51e03a78adf2d0c70b416bd965275b479fc63c1d
Caio Barsi <estagio.ti@granmarquise.com.br>
2026-08-03 17:39:09 -0300

    auto: 2026-08-03 17:39:09

--- src/routes/cadastros.js
+++ src/routes/cadastros.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import { requireAuth, requireSpa, requireWrite } from '../middleware/auth.js';
 import { getFeriados, FERIADOS_FALLBACK } from '../feriados-hub.js';
+import { getAusencias, getSiglasAtivas, AUSENCIAS_FALLBACK } from '../ausencias-hub.js';
 import {
   listarMassagistas, listarMassagistasComStats, listarMassagistasParaPadroes,
   inserirMassagista, atualizarMassagista, deletarMassagista, buscarMassagistaById,
